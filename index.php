05d463e6a9e6a353b803144ef2458245670c48e6
vvictor1 <victoredgar_1@hotmail.com>
2025-11-18 19:21:16 -0300

    mudança de nome

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $burgers = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mundo Burger</title>
+    <title>Mestre Burger</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
