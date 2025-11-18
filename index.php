72dea22518c789237e8916b86141feb3d9633a43
vvictor1 <victoredgar_1@hotmail.com>
2025-11-18 19:36:50 -0300

    teste

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $burgers = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mestre Burger</title>
+    <title>Mestre do Burger</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
